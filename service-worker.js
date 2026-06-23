b401c3a7a7c8705cbf6dfc583356d2f8bc9f2bc9
sehyunka <sehyun.ka@gmail.com>
2026-06-23 22:02:22 +0900

    Add service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+const CACHE = "jh-estimate-v2";
+const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=2", "./manifest.webmanifest", "./icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
+});
