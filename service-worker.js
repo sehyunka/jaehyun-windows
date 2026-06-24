07897158fe6b303a4eeed750831a9171fe7011a5
sehyunka <sehyun.ka@gmail.com>
2026-06-24 09:53:37 +0900

    Update cache to v4

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "jh-estimate-v3";
-const ASSETS = ["./", "./index.html", "./styles.css?v=3", "./app.js?v=3", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "jh-estimate-v4";
+const ASSETS = ["./", "./index.html", "./styles.css?v=4", "./app.js?v=4", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
