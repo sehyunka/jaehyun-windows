b30100d259de770c597fc3ed194261342bef123b
sehyunka <sehyun.ka@gmail.com>
2026-06-24 09:28:48 +0900

    Update cache for share tab

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE = "jh-estimate-v2";
-const ASSETS = ["./", "./index.html", "./styles.css?v=2", "./app.js?v=2", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "jh-estimate-v3";
+const ASSETS = ["./", "./index.html", "./styles.css?v=3", "./app.js?v=3", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
